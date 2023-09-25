a1bf95ddccad333fdd82d99e5709f3792c852d3f
Mateusz Kulesza <ev45ive@gmail.com>
2023-09-25 10:10:29 +0200

    Render func, Virtual DOM, Proxy - Track, Trigger

---
+++ examples/proxy.md
@@ -0,0 +1,32 @@
+## Proxy
+
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Proxy
+
+
+```js
+
+app
+// Proxy(Object) {…}
+//     [[Handler]]: Object
+//     [[Target]]: Object
+//     [[IsRevoked]]: false
+
+proxyV2 = {
+    _secret: '',
+    get value(){
+        console.log('start tracking "value" ')
+        return this._secret
+    },
+    set value(s){
+        console.log('trigger (rerender) "value" ')
+        this._secret = s
+    }
+}
+proxyV2.value = 123
+// set secret
+123
+proxyV2.value
+// get secret
+123
+
+```
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,29 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp, h } from "vue";
+import "./style.css";
+import App from "./App.vue";
 
-createApp(App).mount('#app')
+import HelloWorld from "./components/HelloWorld.vue";
+// createApp(App).mount('#app')
+
+(window as any).app = createApp({
+  // Component provided template option but runtime compilation is not supported in this build of Vue.
+  // Configure your bundler to alias "vue" to "vue/dist/vue.esm-bundler.js".
+  //   template: "<h1>alam kota {{ message }} <HelloWorld/> </h1>",
+  render() {
+    console.log("Re-Rendering...");
+
+    // Virtual DOM:
+    return h("div", null, [
+      h("h1", { class: "Placki" }, "Ala ma kota!"),
+      h("h2", {}, this.message),
+      h("h2", {}, this.placki),
+      h(HelloWorld, { msg: this.message }),
+    ]);
+  },
+  data() {
+    return {
+      message: "Hello Vue!",
+      placki: "123",
+    };
+  },
+}).mount("#app");
