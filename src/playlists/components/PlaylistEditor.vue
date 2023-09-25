583e5e23e49a1e6591e3664085964f4fc7ef0750
Mateusz Kulesza <ev45ive@gmail.com>
2023-09-25 11:48:02 +0200

    Exercise: Create live character counter

--- src/playlists/components/PlaylistEditor.vue
+++ src/playlists/components/PlaylistEditor.vue
@@ -1,12 +1,12 @@
 <template>
   <div>
     <pre>{{ playlist }}</pre>
-    
+
     <form>
       <div class="mb-3">
         <label for="playlistName" class="form-label">Name</label>
         <input type="text" class="form-control" id="playlistName" :value="playlist.name" />
-        <!-- <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div> -->
+        <div class="form-text text-muted float-end">{{ playlist.name.length }} / 100</div>
       </div>
       <div class="mb-3 form-check">
         <input type="checkbox" class="form-check-input" id="playlistPublic" :checked="playlist.public" />
