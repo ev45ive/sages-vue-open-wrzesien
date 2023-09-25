4c47f2273b831a1eacb5e9042c22e9b405ea7e80
Mateusz Kulesza <ev45ive@gmail.com>
2023-09-25 14:09:18 +0200

    Selecting playlists + active CSS classs

--- src/playlists/components/PlaylistList.vue
+++ src/playlists/components/PlaylistList.vue
@@ -1,11 +1,14 @@
 <template>
   <div>
     <div class="list-group">
+      <!-- :class="["list-group-item list-group-item-action",{ active: playlist.id === selectedId }]" -->
       <button
         v-for="(playlist, index) of playlists"
         :key="playlist.id"
         type="button"
-        class="list-group-item list-group-item-action active"
+        class="list-group-item list-group-item-action"
+        :class="{ active: playlist.id === selectedId }"
+        @click="select(playlist.id)"
       >
         {{ index + 1 }}. {{ playlist.name }}
       </button>
@@ -18,13 +21,11 @@ import { ref } from "vue";
 import { mockPlaylists } from "./mockPlaylists";
 
 const playlists = ref(mockPlaylists);
-const selectedId = "234";
+const selectedId = ref("234");
 
 const select = (id: string) => {
-  // TODO: change selection
+  selectedId.value = id;
 };
-
-
 </script>
 
 <style scoped></style>
