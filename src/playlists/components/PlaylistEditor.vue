79893ea102835d3d69c2adb57130ad5cd0661298
Mateusz Kulesza <ev45ive@gmail.com>
2023-09-25 11:42:40 +0200

    Exercise: Bind playlist to form values

--- src/playlists/components/PlaylistEditor.vue
+++ src/playlists/components/PlaylistEditor.vue
@@ -1,21 +1,43 @@
 <template>
-    <div>
-<h3>Editor</h3>
-    </div>
+  <div>
+    <form>
+      <div class="mb-3">
+        <label for="playlistName" class="form-label">Name</label>
+        <input type="text" class="form-control" id="playlistName" />
+        <!-- <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div> -->
+      </div>
+      <div class="mb-3 form-check">
+        <input type="checkbox" class="form-check-input" id="playlistPublic" />
+        <label class="form-check-label" for="playlistPublic">Public</label>
+      </div>
+      <div class="mb-3">
+        <label for="playlistDescription" class="form-label">Description</label>
+        <textarea
+          class="form-control"
+          id="playlistDescription"
+          rows="3"
+        ></textarea>
+      </div>
+      <button type="submit" class="btn btn-primary">Submit</button>
+    </form>
+  </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { defineComponent } from "vue";
 
 export default defineComponent({
-    setup () {
-        
-
-        return {}
-    }
-})
+  data() {
+    return {
+      playlist: {
+        id: "123",
+        name: "Playlist 123",
+        public: true,
+        description: "Best playlist",
+      },
+    };
+  },
+});
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
