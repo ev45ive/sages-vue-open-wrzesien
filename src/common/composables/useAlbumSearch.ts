10c19ad0e1068ab4100dda70ff95972d19389bd5
Mateusz Kulesza <ev45ive@gmail.com>
2023-09-26 13:30:24 +0200

    AbortController, signal, onCleanup, abort

--- src/common/composables/useAlbumSearch.ts
+++ src/common/composables/useAlbumSearch.ts
@@ -1,15 +1,19 @@
-import { Ref, ref, watchEffect } from "vue";
+import { Ref, ref, watch } from "vue";
 
 import { Album } from "../model/Album";
 
 export function useAlbumSearch(query: Ref<string>) {
   const data = ref<Album[]>([]);
 
-  watchEffect(async () => {
-    if (!query.value) return;
+  watch(query, async (query, _, onCleanup) => {
+    if (!query) return;
 
-    const res = await fetch("albums.json");
+    const ctrl = new AbortController();
+
+    const res = await fetch("albums.json", { signal: ctrl.signal });
     data.value = await res.json();
+
+    onCleanup(() => ctrl.abort());
   });
 
   //   watch(query, (query) => {
