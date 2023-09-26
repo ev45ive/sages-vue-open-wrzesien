c8239124c1a2160104110a4a924fc0576d01f607
Mateusz Kulesza <ev45ive@gmail.com>
2023-09-26 08:19:54 +0200

    v-pre, v-once

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,19 @@
 <script setup lang="ts">
+import { ref } from "vue";
 import PlaylistsView from "./playlists/containers/PlaylistsView.vue";
+
+const title = "MusicApp";
+const isOpen = ref(false);
 </script>
 
 <template>
   <div>
+    {{ isOpen }}
     <!-- .container>.row>.col -->
-    <div class="container">
+    <div class="container" v-once>
       <div class="row">
         <div class="col">
-          <h1 class="display-3">Hello Vue</h1>
+          <h1 class="display-3" @click="isOpen = !isOpen">{{ title }}</h1>
           <PlaylistsView />
         </div>
       </div>
