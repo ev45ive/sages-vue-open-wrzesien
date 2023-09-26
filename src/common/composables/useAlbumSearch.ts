a559b8ff5f1407cab9eabda7af316250e1465124
Mateusz Kulesza <ev45ive@gmail.com>
2023-09-26 13:24:07 +0200

    watchEffect +  await fetch("albums.json");

--- src/common/composables/useAlbumSearch.ts
+++ src/common/composables/useAlbumSearch.ts
@@ -1,10 +1,16 @@
 import { Ref, ref, watchEffect } from "vue";
 
-import { mockAlbums } from "../fixtures/mockAlbums";
 import { Album } from "../model/Album";
 
 export function useAlbumSearch(query: Ref<string>) {
   const data = ref<Album[]>([]);
+
+  watchEffect(async () => {
+    if (!query.value) return;
+
+    const res = await fetch("albums.json");
+    data.value = await res.json();
+  });
 
   //   watch(query, (query) => {
   //     if (query) data.value = mockAlbums;
@@ -12,10 +18,10 @@ export function useAlbumSearch(query: Ref<string>) {
   //     // immediate:true
   //   });
 
-  watchEffect(() => {
-    console.log("Query change ", query.value);
-    if (query.value) data.value = mockAlbums;
-  });
+  // watchEffect(() => {
+  //   console.log("Query change ", query.value);
+  //   if (query.value) data.value = mockAlbums;
+  // });
 
   return { data };
 }
